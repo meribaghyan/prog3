c6921158b0bd71e2abee9ebacf657087c2ec7046
meribaghyan <mary.baghyan.y@tumo.org>
2023-02-18 12:45:54 +0400

    lesson4

---
+++ test/server.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+
+var dummyText = "Apple yep";
+
+function main() {
+
+fs.writeFileSync("dummytext.txt", dummyText);
+
+var text = fs.readFileSync("dummytext.txt").toString();
+
+console.log(dummyText == text);
+
+console.log(text);
+
+fs.writeFileSync("undummytext.txt",
+
+text.replace("Apple", "Microsoft")
+
+);
+
+}
+
+main();
